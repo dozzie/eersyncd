f3fd8d8e22327616a1e4a2eb290b75bfa9953ec1
Stanislaw Klekot <dozzie@jarowit.net>
2017-10-14 18:38:46 +0200

    Added skeleton for CLI script and Indira callback modules.

---
+++ src/eersyncd_cli_handler.erl
@@ -0,0 +1,392 @@
+%%%---------------------------------------------------------------------------
+%%% @doc
+%%%   Module that handles command line operations.
+%%%   This includes parsing provided arguments and either starting the daemon
+%%%   or sending it various administrative commands.
+%%%
+%%% @see eersyncd_command_handler
+%%% @end
+%%%---------------------------------------------------------------------------
+
+-module(eersyncd_cli_handler).
+
+-behaviour(gen_indira_cli).
+
+%% interface for daemonizing script
+-export([format_error/1]).
+-export([help/1]).
+
+%% gen_indira_cli callbacks
+-export([parse_arguments/2]).
+-export([handle_command/2, format_request/2, handle_reply/3]).
+
+%% "unused function" warning silencer exports
+-export([println/1, printerr/1, printerr/2]).
+
+%%%---------------------------------------------------------------------------
+%%% types {{{
+
+-define(ADMIN_COMMAND_MODULE, eersyncd_command_handler).
+% XXX: `status' and `stop' commands are bound to few specific errors this
+% module returns; this can't be easily moved to a config/option
+-define(ADMIN_SOCKET_TYPE, indira_unix).
+
+-record(opts, {
+  op :: start | status | stop | reload_config
+      | dist_start | dist_stop,
+  admin_socket :: file:filename(),
+  options :: [{atom(), term()}],
+  args :: [string()]
+}).
+
+%%% }}}
+%%%---------------------------------------------------------------------------
+
+%%%---------------------------------------------------------------------------
+%%% gen_indira_cli callbacks
+%%%---------------------------------------------------------------------------
+
+%%----------------------------------------------------------
+%% parse_arguments() {{{
+
+%% @private
+%% @doc Parse command line arguments and decode opeartion from them.
+
+parse_arguments(Args, [DefAdminSocket, DefConfig] = _Defaults) ->
+  EmptyOptions = #opts{
+    admin_socket = DefAdminSocket,
+    args = [],
+    options = [
+      {config, DefConfig}
+    ]
+  },
+  case indira_cli:folds(fun cli_opt/2, EmptyOptions, Args) of
+    {ok, Options = #opts{op = start}}  -> {ok, start, Options};
+    {ok, Options = #opts{op = status}} -> {ok, status, Options};
+    {ok, Options = #opts{op = stop}}   -> {ok, stop, Options};
+
+    {ok, _Options = #opts{op = undefined}} ->
+      help;
+
+    {ok, _Options = #opts{op = _, args = [_|_]}} ->
+      {error, too_many_args};
+
+    {ok, Options = #opts{op = Command, admin_socket = AdminSocket}} ->
+      {send, {?ADMIN_SOCKET_TYPE, AdminSocket}, Command, Options};
+
+    {error, {help, _Arg}} ->
+      help;
+
+    {error, {Reason, Arg}} ->
+      {error, {Reason, Arg}}
+  end.
+
+%% }}}
+%%----------------------------------------------------------
+%% handle_command() {{{
+
+%% @private
+%% @doc Execute commands more complex than "request -> reply -> print".
+
+handle_command(start = _Command,
+               _Options = #opts{admin_socket = _Socket, options = _CLIOpts}) ->
+  'TODO';
+
+handle_command(status = Command,
+               Options = #opts{admin_socket = Socket, options = CLIOpts}) ->
+  Timeout = proplists:get_value(timeout, CLIOpts, infinity),
+  Opts = case proplists:get_bool(wait, CLIOpts) of
+    true  = Wait -> [{timeout, Timeout}, retry];
+    false = Wait -> [{timeout, Timeout}]
+  end,
+  {ok, Request} = format_request(Command, Options),
+  case indira_cli:send_one_command(?ADMIN_SOCKET_TYPE, Socket, Request, Opts) of
+    {ok, Reply} ->
+      handle_reply(Reply, Command, Options);
+    {error, Reason} when Reason == timeout, Wait;
+                         Reason == econnrefused;
+                         Reason == enoent ->
+      % FIXME: what to do when a command got sent, but no reply was received?
+      % (Reason == timeout)
+      Reply = ?ADMIN_COMMAND_MODULE:hardcoded_reply(daemon_stopped),
+      handle_reply(Reply, Command, Options);
+    {error, Reason} ->
+      {error, {send, Reason}} % mimic what `indira_cli:execute()' returns
+  end;
+
+handle_command(stop = Command,
+               Options = #opts{admin_socket = Socket, options = CLIOpts}) ->
+  Timeout = proplists:get_value(timeout, CLIOpts, infinity),
+  Opts = [{timeout, Timeout}],
+  {ok, Request} = format_request(Command, Options),
+  case indira_cli:send_one_command(?ADMIN_SOCKET_TYPE, Socket, Request, Opts) of
+    {ok, Reply} ->
+      handle_reply(Reply, Command, Options);
+    {error, Reason} when Reason == closed;
+                         Reason == econnrefused;
+                         Reason == enoent ->
+      Reply = ?ADMIN_COMMAND_MODULE:hardcoded_reply(generic_ok),
+      handle_reply(Reply, Command, Options);
+    {error, Reason} ->
+      {error, {send, Reason}} % mimic what `indira_cli:execute()' returns
+  end.
+
+%% }}}
+%%----------------------------------------------------------
+%% format_request() + handle_reply() {{{
+
+%% @private
+%% @doc Format a request to send to daemon.
+
+format_request(status = _Command, _Options = #opts{options = CLIOpts}) ->
+  Request = case proplists:get_bool(wait, CLIOpts) of
+    true  -> ?ADMIN_COMMAND_MODULE:format_request(status_wait);
+    false -> ?ADMIN_COMMAND_MODULE:format_request(status)
+  end,
+  {ok, Request};
+format_request(Command, _Options) ->
+  Request = ?ADMIN_COMMAND_MODULE:format_request(Command),
+  {ok, Request}.
+
+%% @private
+%% @doc Handle a reply to a command sent to daemon.
+
+handle_reply(Reply, status = Command, _Options) ->
+  % `status' and `status_wait' have the same `Command' and replies
+  case ?ADMIN_COMMAND_MODULE:parse_reply(Reply, Command) of
+    running ->
+      println("eersyncd is running"),
+      ok;
+    stopped ->
+      println("eersyncd is stopped"),
+      {error, 1};
+    % for future changes in status detection
+    Status ->
+      {error, {unknown_status, Status}}
+  end;
+
+handle_reply(Reply, stop = Command, _Options = #opts{options = CLIOpts}) ->
+  PrintPid = proplists:get_bool(print_pid, CLIOpts),
+  case ?ADMIN_COMMAND_MODULE:parse_reply(Reply, Command) of
+    {ok, Pid} when PrintPid -> println(Pid), ok;
+    {ok, _Pid} when not PrintPid -> ok;
+    ok -> ok;
+    {error, Reason} -> {error, Reason}
+  end;
+
+handle_reply(Reply, reload_config = Command, _Options) ->
+  case ?ADMIN_COMMAND_MODULE:parse_reply(Reply, Command) of
+    ok ->
+      ok;
+    {error, Message} when is_binary(Message) ->
+      printerr(["reload error: ", Message]),
+      {error, 1};
+    {error, Errors} when is_list(Errors) ->
+      printerr("reload errors:"),
+      lists:foreach(
+        fun({Part, Error}) -> printerr(["  ", Part, ": ", Error]) end,
+        Errors
+      ),
+      {error, 1}
+  end;
+
+handle_reply(Reply, Command, _Options) ->
+  ?ADMIN_COMMAND_MODULE:parse_reply(Reply, Command).
+
+%% }}}
+%%----------------------------------------------------------
+
+%%%---------------------------------------------------------------------------
+%%% interface for daemonizing script
+%%%---------------------------------------------------------------------------
+
+%%----------------------------------------------------------
+%% format_error() {{{
+
+%% @doc Convert an error to a printable form.
+
+-spec format_error(term()) ->
+  iolist().
+
+format_error(Reason) when is_binary(Reason) ->
+  Reason;
+format_error(Reason) ->
+  ["error message TODO; error: ", io_lib:print(Reason, 1, 16#ffffffff, -1)].
+
+%% }}}
+%%----------------------------------------------------------
+%% help() {{{
+
+%% @doc Return a printable help message.
+
+-spec help(string()) ->
+  iolist().
+
+help(ScriptName) ->
+  _Usage = [
+    "Experimental rsync daemon.\n",
+    "Controlling daemon:\n",
+    "  ", ScriptName, " [--socket <path>] start [--debug] [--config <path>] [--pidfile <path>]\n",
+    "  ", ScriptName, " [--socket <path>] status [--wait [--timeout <seconds>]]\n",
+    "  ", ScriptName, " [--socket <path>] stop [--timeout <seconds>] [--print-pid]\n",
+    "  ", ScriptName, " [--socket <path>] reload\n",
+    "Distributed Erlang support:\n",
+    "  ", ScriptName, " [--socket <path>] dist-erl-start\n",
+    "  ", ScriptName, " [--socket <path>] dist-erl-stop\n",
+    ""
+  ].
+
+%% }}}
+%%----------------------------------------------------------
+
+%%%---------------------------------------------------------------------------
+%%% various helpers
+%%%---------------------------------------------------------------------------
+
+%% @doc Command line argument parser for {@link indira_cli:folds/3}.
+
+cli_opt("-h" = _Arg, _Opts) ->
+  {error, help};
+cli_opt("--help" = _Arg, _Opts) ->
+  {error, help};
+
+cli_opt("--socket=" ++ Socket = _Arg, Opts) ->
+  cli_opt(["--socket", Socket], Opts);
+cli_opt("--socket" = _Arg, _Opts) ->
+  {need, 1};
+cli_opt(["--socket", Socket] = _Arg, Opts) ->
+  _NewOpts = Opts#opts{admin_socket = Socket};
+
+cli_opt("--pidfile=" ++ Path = _Arg, Opts) ->
+  cli_opt(["--pidfile", Path], Opts);
+cli_opt("--pidfile" = _Arg, _Opts) ->
+  {need, 1};
+cli_opt(["--pidfile", Path] = _Arg, Opts = #opts{options = CLIOpts}) ->
+  _NewOpts = Opts#opts{options = [{pidfile, Path} | CLIOpts]};
+
+cli_opt("--config=" ++ Path = _Arg, Opts) ->
+  cli_opt(["--config", Path], Opts);
+cli_opt("--config" = _Arg, _Opts) ->
+  {need, 1};
+cli_opt(["--config", Path] = _Arg, Opts = #opts{options = CLIOpts}) ->
+  _NewOpts = Opts#opts{options = [{config, Path} | CLIOpts]};
+
+cli_opt("--timeout=" ++ Timeout = _Arg, Opts) ->
+  cli_opt(["--timeout", Timeout], Opts);
+cli_opt("--timeout" = _Arg, _Opts) ->
+  {need, 1};
+cli_opt(["--timeout", Timeout] = _Arg, Opts = #opts{options = CLIOpts}) ->
+  case make_integer(Timeout) of
+    {ok, Seconds} when Seconds > 0 ->
+      % NOTE: we need timeout in milliseconds
+      _NewOpts = Opts#opts{options = [{timeout, Seconds * 1000} | CLIOpts]};
+    _ ->
+      {error, bad_timeout}
+  end;
+
+cli_opt("--debug" = _Arg, Opts = #opts{options = CLIOpts}) ->
+  _NewOpts = Opts#opts{options = [{debug, true} | CLIOpts]};
+
+cli_opt("--wait" = _Arg, Opts = #opts{options = CLIOpts}) ->
+  _NewOpts = Opts#opts{options = [{wait, true} | CLIOpts]};
+
+cli_opt("--print-pid" = _Arg, Opts = #opts{options = CLIOpts}) ->
+  _NewOpts = Opts#opts{options = [{print_pid, true} | CLIOpts]};
+
+cli_opt("-" ++ _ = _Arg, _Opts) ->
+  {error, bad_option};
+
+cli_opt(Arg, Opts = #opts{op = Op, args = OpArgs}) when Op /= undefined ->
+  % `++' operator is a little costly, but considering how many arguments there
+  % will be, the time it all takes will drown in Erlang startup
+  _NewOpts = Opts#opts{args = OpArgs ++ [Arg]};
+
+cli_opt(Arg, Opts = #opts{op = undefined}) ->
+  case Arg of
+    "start"  -> Opts#opts{op = start};
+    "status" -> Opts#opts{op = status};
+    "stop"   -> Opts#opts{op = stop};
+    "reload" -> Opts#opts{op = reload_config};
+    "dist-erl-start" -> Opts#opts{op = dist_start};
+    "dist-erl-stop"  -> Opts#opts{op = dist_stop};
+    _ -> {error, bad_command}
+  end.
+
+%%%---------------------------------------------------------------------------
+%%% data conversion helpers
+
+%% @doc Helper to convert string to integer.
+%%
+%%   Doesn't die on invalid argument.
+
+-spec make_integer(string()) ->
+  {ok, integer()} | {error, badarg}.
+
+make_integer(String) ->
+  try list_to_integer(String) of
+    Integer -> {ok, Integer}
+  catch
+    error:badarg -> {error, badarg}
+  end.
+
+%%%---------------------------------------------------------------------------
+%%% printing helpers
+
+%% @private
+%% @doc Print a string to STDOUT, ending it with a new line.
+
+-spec println(iolist() | binary()) ->
+  ok.
+
+println(Line) ->
+  io:put_chars([Line, $\n]).
+
+%% @private
+%% @doc Print an error to STDERR, ending it with a new line.
+
+-spec printerr(iolist() | binary()) ->
+  ok.
+
+printerr(Line) ->
+  printerr(Line, []).
+
+%% @private
+%% @doc Print an error (with some context) to STDERR, ending it with a new
+%%   line.
+
+-spec printerr(iolist() | binary(), [{Name, Value}]) ->
+  ok
+  when Name :: atom(),
+       Value :: atom() | string() | binary() | integer().
+
+printerr(Line, InfoFields) ->
+  Info = [
+    [" ", format_info_field(Name, Value)] ||
+    {Name, Value} <- InfoFields
+  ],
+  case Info of
+    [] -> io:put_chars(standard_error, [Line, $\n]);
+    _  -> io:put_chars(standard_error, [Line, $:, Info, $\n])
+  end.
+
+%% @doc Helper for {@link printerr/2}.
+
+-spec format_info_field(atom(), Value) ->
+  iolist()
+  when Value :: atom() | string() | binary() | integer().
+
+format_info_field(Name, Value) when is_list(Value); is_binary(Value) ->
+  case re:run(Value, "^[a-zA-Z0-9~@_+:,./-]*$", [{capture, none}]) of
+    match ->
+      [atom_to_list(Name), $=, Value];
+    nomatch ->
+      PrintValue = re:replace(Value, "[\"\\\\]", "\\\\&", [global]),
+      [atom_to_list(Name), $=, $", PrintValue, $"]
+  end;
+format_info_field(Name, Value) when is_integer(Value) ->
+  [atom_to_list(Name), $=, integer_to_list(Value)];
+format_info_field(Name, Value) when is_atom(Value) ->
+  [atom_to_list(Name), $=, atom_to_list(Value)].
+
+%%%---------------------------------------------------------------------------
+%%% vim:ft=erlang:foldmethod=marker
---
+++ src/eersyncd_command_handler.erl
@@ -0,0 +1,177 @@
+%%%---------------------------------------------------------------------------
+%%% @doc
+%%%   Administrative commands handler for Indira.
+%%%
+%%% @see eersyncd_cli_handler
+%%% @end
+%%%---------------------------------------------------------------------------
+
+-module(eersyncd_command_handler).
+
+-behaviour(gen_indira_command).
+
+%% gen_indira_command callbacks
+-export([handle_command/2]).
+
+%% interface for eersyncd_cli_handler
+-export([format_request/1, parse_reply/2, hardcoded_reply/1]).
+
+%%%---------------------------------------------------------------------------
+%%% gen_indira_command callbacks
+%%%---------------------------------------------------------------------------
+
+%% @private
+%% @doc Handle administrative commands sent to the daemon.
+
+handle_command([{<<"command">>, <<"status">>}, {<<"wait">>, false}] = _Command,
+               _Args) ->
+  case is_started() of
+    true  -> [{result, running}];
+    false -> [{result, stopped}]
+  end;
+handle_command([{<<"command">>, <<"status">>}, {<<"wait">>, true}] = _Command,
+               _Args) ->
+  case wait_for_start() of
+    true  -> [{result, running}];
+    false -> [{result, stopped}]
+  end;
+
+handle_command([{<<"command">>, <<"stop">>}] = _Command, _Args) ->
+  log_info(stop, "stopping eersyncd daemon", []),
+  init:stop(),
+  [{result, ok}, {pid, list_to_binary(os:getpid())}];
+
+handle_command([{<<"command">>, <<"reload_config">>}] = _Command, _Args) ->
+  log_info(reload, "reloading configuration", []),
+  [{result, todo}];
+
+handle_command([{<<"command">>, <<"dist_start">>}] = _Command, _Args) ->
+  log_info(dist_start, "starting Erlang networking", []),
+  case indira_app:distributed_start() of
+    ok ->
+      [{result, ok}];
+    {error, Reason} ->
+      log_error(dist_start, "can't setup Erlang networking",
+                [{error, {term, Reason}}]),
+      [{result, error}, {message, <<"Erlang networking error">>}]
+  end;
+
+handle_command([{<<"command">>, <<"dist_stop">>}] = _Command, _Args) ->
+  log_info(dist_stop, "stopping Erlang networking", []),
+  case indira_app:distributed_stop() of
+    ok ->
+      [{result, ok}];
+    {error, Reason} ->
+      log_error(dist_stop, "can't shutdown Erlang networking",
+                [{error, {term, Reason}}]),
+      [{result, error}, {message, <<"Erlang networking error">>}]
+  end;
+
+handle_command(Command, _Args) ->
+  % `Command' is a structure coming from JSON, so it's safe to log it as it is
+  eersyncd_log:warn(unknown_command, "unknown command", [{command, Command}]),
+  [{result, error}, {message, <<"unrecognized command">>}].
+
+%%%---------------------------------------------------------------------------
+%%% interface for eersyncd_cli_handler
+%%%---------------------------------------------------------------------------
+
+%% @doc Encode administrative command as a serializable structure.
+
+-spec format_request(gen_indira_cli:command()) ->
+  gen_indira_cli:request().
+
+format_request(status        = Command) -> [{command, Command}, {wait, false}];
+format_request(status_wait   =_Command) -> [{command, status}, {wait, true}];
+format_request(stop          = Command) -> [{command, Command}];
+format_request(reload_config = Command) -> [{command, Command}];
+format_request(dist_start    = Command) -> [{command, Command}];
+format_request(dist_stop     = Command) -> [{command, Command}].
+
+%% @doc Decode a reply to an administrative command.
+
+-spec parse_reply(gen_indira_cli:reply(), gen_indira_cli:command()) ->
+  term().
+
+%% generic replies
+parse_reply([{<<"result">>, <<"ok">>}] = _Reply, _Command) ->
+  ok;
+parse_reply([{<<"result">>, <<"todo">>}] = _Reply, _Command) ->
+  {error, 'TODO'};
+parse_reply([{<<"message">>, Message}, {<<"result">>, <<"error">>}] = _Reply,
+            _Command) ->
+  {error, Message};
+
+parse_reply([{<<"pid">>, Pid}, {<<"result">>, <<"ok">>}] = _Reply,
+            stop = _Command) ->
+  {ok, Pid};
+
+parse_reply([{<<"result">>, <<"running">>}]  = _Reply, status = _Command) ->
+  running;
+parse_reply([{<<"result">>, <<"stopped">>}]  = _Reply, status = _Command) ->
+  stopped;
+
+%% unrecognized reply
+parse_reply(_Reply, _Command) ->
+  {error, unrecognized_reply}.
+
+%% @doc Artificial replies for `eersyncd_cli_handler' when no reply was
+%%   received.
+
+-spec hardcoded_reply(generic_ok | daemon_stopped) ->
+  gen_indira_cli:reply().
+
+hardcoded_reply(daemon_stopped = _Event) -> [{<<"result">>, <<"stopped">>}];
+hardcoded_reply(generic_ok     = _Event) -> [{<<"result">>, <<"ok">>}].
+
+%%%---------------------------------------------------------------------------
+
+%%----------------------------------------------------------
+%% waiting for/checking daemon's start {{{
+
+wait_for_start() ->
+  % XXX: this will wait until the children of top-level supervisor all
+  % started (and each child supervisor waits for its children, transitively)
+  % or the supervisor shuts down due to an error
+  try supervisor:which_children(eersyncd_sup) of
+    _ -> true
+  catch
+    _:_ -> false
+  end.
+
+is_started() ->
+  % `{AppName :: atom(), Desc :: string(), Version :: string()}' or `false';
+  % only non-false when the application started successfully (it's still
+  % `false' during boot time)
+  AppEntry = lists:keyfind(eersyncd, 1, application:which_applications()),
+  AppEntry /= false.
+
+%% }}}
+%%----------------------------------------------------------
+
+%%%---------------------------------------------------------------------------
+
+-spec log_info(atom(), eersyncd_log:event_message(),
+               eersyncd_log:event_info()) ->
+  ok.
+
+log_info(Command, Message, Context) ->
+  eersyncd_log:info(command, Message, Context ++ [{command, {term, Command}}]).
+
+-spec log_error(atom(), eersyncd_log:event_message(),
+                eersyncd_log:event_info()) ->
+  ok.
+
+log_error(Command, Message, Context) ->
+  eersyncd_log:warn(command, Message, Context ++ [{command, {term, Command}}]).
+
+%-spec format_term(any()) ->
+%  binary().
+%
+%format_term(Term) when is_binary(Term) ->
+%  Term;
+%format_term(Term) ->
+%  iolist_to_binary(io_lib:print(Term, 1, 16#ffffffff, -1)).
+
+%%%---------------------------------------------------------------------------
+%%% vim:ft=erlang:foldmethod=marker
